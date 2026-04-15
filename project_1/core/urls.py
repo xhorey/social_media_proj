a1e90ad314f60877477290ef79899b0f431e8881
Саша Дубівка <xhoreycode@gmail.com>
2026-04-14 21:59:14 -0400

    Urls for home, signup and sigin/ HTML files fixes

--- project_1/core/urls.py
+++ project_1/core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='index')
+    path('', views.home, name='home'),
+    path('signin', views.signin, name='signin'),
+    path('signup', views.signup, name='signup'),
 ]
--- project_1/core/views.py
+++ project_1/core/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def login(request):
+def signin(request):
     return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'Main_Web_Page.html')
+
+def signup(request):
+    return render(request, 'Signup.html')

9485ca95cb9565d8ba13ee8fa41d60acbcc44793
xhorey <xhoreycode@gmail.com>
2026-04-16 22:15:19 -0400

    Profile model

--- project_1/core/models.py
+++ project_1/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank_pfp.jpg')
+
+    def __str__(self):
+        return self.user.username
+

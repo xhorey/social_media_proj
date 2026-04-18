bf64d147ce2bc693604c1f125077758e9c064de6
Саша Дубівка <xhoreycode@gmail.com>
2026-04-17 20:31:11 -0400

    Working signup backend

--- project_1/core/views.py
+++ project_1/core/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 from django.http import HttpResponse
+from .models import Profile
 
 
 def signin(request):
@@ -9,4 +12,32 @@ def home(request):
     return render(request, 'Main_Web_Page.html')
 
 def signup(request):
-    return render(request, 'Signup.html')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmPassword = request.POST['confirmPassword']
+
+        if confirmPassword == password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken.')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username taken.')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signin')
+        else:
+            messages.info(request, 'Password is not matching!')
+            return redirect('signup')
+
+    else:
+
+        return render(request, 'Signup.html')

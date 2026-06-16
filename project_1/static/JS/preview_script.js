3ae9ddd0668c205f277bb20b648ab7adcff7c119
xhorey <xhoreycode@gmail.com>
2026-06-16 12:57:29 -0400

    Normal design of posting page

--- project_1/static/JS/preview_script.js
+++ project_1/static/JS/preview_script.js
@@ -2,6 +2,24 @@ const selectImageInput = document.getElementById('selectImage');
 const aspectSelect = document.getElementById('aspect_ratio');
 const previewContainer = document.getElementById('imagePreviewContainer');
 const previewImg = document.getElementById('imagePreview');
+const imageInput = document.getElementById('selectImage');
+const imageButton = document.getElementById('selectImageButton');
+const divSelector = document.querySelector('.ratio-selector'); 
+const deleteImgButton = document.getElementById('deleteImageButton');
+
+
+imageButton.addEventListener('click', () => {
+    imageInput.click();
+});
+
+deleteImgButton.addEventListener('click', () => {
+    imageInput.value = '';
+    previewContainer.style.display = 'none';
+    previewImg.src = '';
+    divSelector.classList.add('hidden');
+    deleteImgButton.classList.add('hidden');
+    imageButton.textContent = "Add Image"
+});
 
 selectImageInput.addEventListener('change', function() {
     const file = this.files[0];
@@ -11,9 +29,16 @@ selectImageInput.addEventListener('change', function() {
         previewImg.src = objectURL;
         previewContainer.style.display = 'block';
         previewImg.onload = () => URL.revokeObjectURL(objectURL);
+        divSelector.classList.remove('hidden');
+        deleteImgButton.classList.remove('hidden');
+        imageButton.textContent = "Change Image"
+
     } else{
         previewContainer.style.display = 'none';
         previewImg.src = '';
+        divSelector.classList.add('hidden');
+        deleteImgButton.classList.add('hidden');
+        imageButton.textContent = "Add Image"
     }
 });
 
